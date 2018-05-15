48ee6cbbc17523e8cb399190cbc903b928468bfe
Rick Byers <rick@rbyers.net>
2018-05-14 22:21:22 -0400

    Implement log

--- app.js
+++ app.js
@@ -14,13 +14,13 @@ const datastore = Datastore();
 async function getSession() {
 	const sessionKey = datastore.key(['Session', 'default']);
 	
-	var sessions = await datastore.get(sessionKey);
+	const sessions = await datastore.get(sessionKey);
 	if (sessions[0]) { 
 		// TODO handle session expiry
 		return sessions[0];
 	}
 
-	var session = await login();
+	const session = await login();
 	session.device_serial = await getDevice(session);
 	
 	await datastore.upsert({key: sessionKey, data: session});
@@ -28,7 +28,7 @@ async function getSession() {
 }
 
 async function login() {
-	var secrets = JSON.parse(fs.readFileSync(__dirname + "/secrets.json"));
+	const secrets = JSON.parse(fs.readFileSync(__dirname + "/secrets.json"));
 
 	const response = await fetch('https://support.iaqualink.com/users/sign_in.json', {
 		body: JSON.stringify({
@@ -46,7 +46,7 @@ async function login() {
 		console.error('Unexpected signin response: ', json);
 		throw new Error('sign_in.json failure - unexpected response');
 	}
-	var s = {
+	const s = {
 		id: json.session_id,
 		user_id: json.id,
 		authentication_token: json.authentication_token};
@@ -55,13 +55,13 @@ async function login() {
 }
 
 async function getDevice(session) {
-	var url = 'https://support.iaqualink.com/devices.json' + 
+	const url = 'https://support.iaqualink.com/devices.json' + 
 		'?api_key=' + api_key +
 		'&authentication_token=' + session.authentication_token +
 		'&user_id=' + session.user_id;
 	const response = await fetch(url);
 	if (response.status !== 200) {
-		var body = await response.text();
+		const body = await response.text();
 		console.error('devices.json failure', url, response.status, response.statusText, body);
 		throw new Error('devices.json failure:' + response.status + ' ' + response.statusText);
 	}
@@ -74,26 +74,26 @@ async function getDevice(session) {
 }
 
 async function getTemps(session) {
-	var url = 'https://iaqualink-api.realtime.io/v1/mobile/session.json' +
+	const url = 'https://iaqualink-api.realtime.io/v1/mobile/session.json' +
 		'?actionID=command' +
 		'&command=get_home' +
 		'&serial=' + session.device_serial +
 		'&sessionID=' + session.id;
 		const response = await fetch(url);
 	if (response.status !== 200) {
-		var body = await response.text();
+		const body = await response.text();
 		console.error('session.json failure', url, response.status, response.statusText, body);
 		throw new Error('session.json failure:' + response.status + ' ' + response.statusText);
 	}
 	const json = await response.json();
 	
 	// Convert array of key/value pairs into an object
-	var items = Object.assign({}, ...json.home_screen);
+	const items = Object.assign({}, ...json.home_screen);
 	
 	// Compute heater temperature.
 	// "1" means heating, "3" means on but not heating
 	// "spa" (temp 1) seems to take precedence when it's on
-	var heater = 0;
+	let heater = 0;
 	if (items.spa_heater==="1")
 		heater = parseInt(items.spa_set_point, 10);
 	else if(items.pool_heater==="1")
@@ -106,14 +106,14 @@ async function getTemps(session) {
 }
 
 async function update() {
-	var session = await getSession();
-	var temps = await getTemps(session);
+	const session = await getSession();
+	const temps = await getTemps(session);
 	
 	// Get the most recent database entry
 	const latestTempsKey = datastore.key(['Temps', 'latest']);
-	var results = await datastore.get(latestTempsKey);	
+	const results = await datastore.get(latestTempsKey);	
 	if (results[0]) {
-		var lt = results[0];
+		const lt = results[0];
 		if (temps.air === lt.air && temps.pool === lt.pool && temps.heater === lt.heater)
 			return 'No change: ' + JSON.stringify(temps);
 	}
@@ -126,18 +126,45 @@ async function update() {
 	return 'Added entry: ' + JSON.stringify(temps);
 }
 
+async function log(response) {
+	const query = datastore.createQuery('Temps')
+    	.order('timestamp', { descending: true });
+	const results = await datastore.runQuery(query);
+	
+	// Create the response in a string.
+	// If this gets really big we could setup a streaming response,
+	// and use a query cursor.
+	let csv = 'timesamp, air, pool, heater\n';
+    for(let temps of results[0])
+    	csv += `${temps.timestamp}, ${temps.air}, ${temps.pool}, ${temps.heater}\n`;
+	
+	response
+		.status(200)
+        .set('Content-Type', 'text/csv')
+     	.send(csv)
+     	.end();
+}
+
 app.get('/', (req, res) => {
 	res.sendFile(__dirname + "/static/index.html");
 });
 
 app.get('/update', (req, res) => {
-	update(res).then(msg => {
+	update().then(msg => {
 		res.status(200).send(msg).end();
 	}).catch(error => {
 		console.error(error);
 		res.status(500).send('Server error!');
 	});
 });
+
+app.get('/log.csv', (req, res) => {
+	log(res).catch(error => {
+		console.error(error);
+		res.status(500).send('Server error!');
+	});
+});
+
 
 // Start the server
 const PORT = process.env.PORT || 8080;
